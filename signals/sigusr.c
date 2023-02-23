afa098a5069a6de0bc131967647ccc4249cb259d
Bin Zhao <bssrdf@gmail.com>
2023-02-22 21:11:25 -0500

    add another signal catch

--- signals/sigusr.c
+++ signals/sigusr.c
@@ -9,6 +9,14 @@ main(void)
 		err_sys("can't catch SIGUSR1");
 	if (signal(SIGUSR2, sig_usr) == SIG_ERR)
 		err_sys("can't catch SIGUSR2");
+    /*if (signal(SIGTERM, sig_usr) == SIG_ERR)
+		err_sys("can't catch SIGTERM");*/
+    void (*sig_usr_pr)(int);
+    sig_usr_pr = sig_usr;
+	if (signal(SIGTERM, sig_usr_pr) == SIG_ERR)
+		err_sys("can't catch SIGTERM");
+	/*if (signal(SIGINT, sig_usr_pr) == SIG_ERR)
+		err_sys("can't catch SIGTERM");*/
 	for ( ; ; )
 		pause();
 }
@@ -20,6 +28,10 @@ sig_usr(int signo)		/* argument is signal number */
 		printf("received SIGUSR1\n");
 	else if (signo == SIGUSR2)
 		printf("received SIGUSR2\n");
+	else if (signo == SIGTERM)
+		printf("received SIGTERM\n");
+	else if (signo == SIGINT)
+		printf("received SIGINT\n");
 	else
 		err_dump("received signal %d\n", signo);
 }
